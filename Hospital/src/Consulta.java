2258a16795dcd42702c2e8bca305fa6bc05145eb
davikkjjkk <davilqdavi@gmail.com>
2025-10-02 13:47:19 -0300

    Criação da classe consulta

---
+++ Hospital/src/Consulta.java
@@ -0,0 +1,58 @@
+import java.time.LocalDateTime;
+
+public class Consulta {
+    private Paciente paciente;
+    private Medico medico;
+    private LocalDateTime dataHora;
+    private String local;
+    private String status;
+    private double valor;
+
+    public Consulta(Paciente paciente, Medico medico, LocalDateTime dataHora, String local, double valor){
+        this.paciente = paciente;
+        this.medico = medico;
+        this.dataHora = dataHora;
+        this.local = local;
+        this.status = "Agendada";
+        this.valor = aplicarDesconto(valor);    
+    }
+
+    private double aplicarDesconto(double valorBase){
+        if(paciente.isEspecial() && paciente instanceof PacienteEspecial){
+            PacienteEspecial p = (PacienteEspecial) paciente;
+            double desconto = p.getPlanoSaude().getDesconto();
+            return valorBase - (valorBase * desconto);
+        }
+        return valorBase;
+    }
+
+    public Paciente getPaciente(){
+        return paciente;
+    }
+
+    public Medico getMedico(){
+        return medico;
+    }
+
+    public LocalDateTime getDataHora(){
+        return dataHora;
+    }
+
+    public String getLocal(){
+        return local;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public double getValor(){
+        return valor;
+    }
+
+
+}
